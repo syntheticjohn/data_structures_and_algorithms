652b7165610f7ead42d7ac3baa2b2e74c7fe79a7
syntheticjohn <jlee.sbu@gmail.com>
2020-11-03 15:40:30 -0500

    updated driver code

--- hash_tables.py
+++ hash_tables.py
@@ -6,9 +6,9 @@ class HashTable(object):
 
     def lookup(self, key):
         """ Look up value from array by its key """ 
-        index = hash(key) % self.length
-        bucket = self.array[index]
-        if not self.array[index]:
+        hash_key = hash(key) % self.length
+        bucket = self.array[hash_key]
+        if not self.array[hash_key]:
             raise KeyError() 
         else:
             for idx, key_val_pair in enumerate(bucket):
@@ -18,8 +18,8 @@ class HashTable(object):
 
     def insert(self, key, value):
         """ Insert a value to array by its key """
-        index = hash(key) % self.length
-        bucket = self.array[index]
+        hash_key = hash(key) % self.length
+        bucket = self.array[hash_key]
         key_exists = False
         for idx, key_val_pair in enumerate(bucket):
             k, v = key_val_pair
@@ -33,19 +33,19 @@ class HashTable(object):
 
     def delete(self, key):   
         """ Delete a value from array by its key """
-        index = hash(key) % self.length
-        if self.array[index] is None:
-            return "Value does not exist at index"
+        hash_key = hash(key) % self.length
+        if self.array[hash_key] is None:
+            return "Value does not exist at hash_key"
         else:
-            for key_val_pair in self.array[index]:
+            for key_val_pair in self.array[hash_key]:
                 if key_val_pair[0] == key:
-                    self.array[index].remove(key_val_pair)
+                    self.array[hash_key].remove(key_val_pair)
 
     def update(self, key, value):
         """ Update a value in array by its key """
-        index = hash(key) % self.length
-        if self.array[index] is None:
-            return "No value to update at index"
+        hash_key = hash(key) % self.length
+        if self.array[hash_key] is None:
+            return "No value to update at hash_key"
         else:
             for key_val_pair in self.array[index]:
                 if key_val_pair[0] == key:
@@ -54,10 +54,10 @@ class HashTable(object):
 
 if __name__ == "__main__":
     hash_table = HashTable(length=5)
-    hash_table.insert(5, 'USA')    
-    hash_table.insert(5, 'Canada')
-    hash_table.insert(5, 'Germany')
-    hash_table.insert(22, 'France')
-    # hash_table.delete(5)
-    # hash_table.update(5, 'Japan')
+    hash_table.insert(10, 'USA')    
+    hash_table.insert(20, 'Canada')
+    hash_table.insert(25, 'Germany')
+    hash_table.insert(32, 'France')
+    hash_table.delete(20)
+    hash_table.update(5, 'Japan')
     print(hash_table.array)
