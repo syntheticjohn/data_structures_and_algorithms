6c9034f22dcd011024d4607b28bed3fbbeb9fc37
syntheticjohn <jlee.sbu@gmail.com>
2020-11-21 11:24:28 -0500

    added raise ValueError for delete and update functions

--- hash_tables.py
+++ hash_tables.py
@@ -5,13 +5,13 @@ class HashTable(object):
         self.length = length
         self.array = [[] for _ in range(length)]
 
-    def lookup(self, key):
+    def lookup(self, key, default=None):
         """ Look up value from array by its key """
         hash_key = hash(key) % self.length
         bucket = self.array[hash_key]
         if not bucket:
-            raise KeyError()
-        for idx, key_val_pair in enumerate(bucket):
+            return default
+        for key_val_pair in bucket:
             k, v = key_val_pair
             if k == key:
                 return v
@@ -20,7 +20,7 @@ class HashTable(object):
         """ Insert a value to array by its key """
         hash_key = hash(key) % self.length
         bucket = self.array[hash_key]
-        for idx, key_val_pair in enumerate(bucket):
+        for idx, key_val_pair in bucket:
             k, v = key_val_pair
             if k == key:
                 bucket[idx] = [key, value]
@@ -30,19 +30,20 @@ class HashTable(object):
     def delete(self, key):
         """ Delete a value from array by its key """
         hash_key = hash(key) % self.length
-        if self.array[hash_key] is None:
-            return "Value does not exist at hash_key"
-        else:
-            for key_val_pair in self.array[hash_key]:
-                if key_val_pair[0] == key:
-                    self.array[hash_key].remove(key_val_pair)
+        bucket = self.array[hash_key]
+        if not bucket:
+            raise ValueError('Key does not exist')
+        for key_val_pair in bucket:
+            if key_val_pair[0] == key:
+                bucket.remove(key_val_pair)
 
     def update(self, key, value):
         """ Update a value in array by its key """
         hash_key = hash(key) % self.length
-        if self.array[hash_key] is None:
-            return "No value to update at hash_key"
-        for key_val_pair in self.array[hash_key]:
+        bucket = self.array[hash_key]
+        if not bucket:
+            raise ValueError('Key does not exist')
+        for key_val_pair in bucket:
             if key_val_pair[0] == key:
                 key_val_pair[1] = value
                 break
@@ -52,6 +53,7 @@ if __name__ == "__main__":
     hash_table = HashTable(length=5)
     hash_table.insert(10, 'USA')
     hash_table.insert(20, 'Canada')
+    hash_table.insert(20, 'Mexico')
     hash_table.insert(25, 'Germany')
     hash_table.insert(32, 'France')
     hash_table.delete(20)
